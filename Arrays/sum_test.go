61f56540a75a0db9d560d7214963a9239ce00c9f
Hopertz <lugano.ngulwa@gmail.com>
2022-11-11 12:10:03 +0300

    Add fun SumAll and tests

--- Arrays/sum_test.go
+++ Arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -15,4 +18,11 @@ func TestSum(t *testing.T) {
 
 	})
 
+}
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
