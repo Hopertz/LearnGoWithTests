ee8eda75fd74aa82e3065e524d94341cb94f8413
Hopertz <lugano.ngulwa@gmail.com>
2022-12-22 06:14:10 +0300

    Remove unused Area function

--- StructsMethodsInterfaces/shapes.go
+++ StructsMethodsInterfaces/shapes.go
@@ -36,7 +36,3 @@ func (r Rectangle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
-}
