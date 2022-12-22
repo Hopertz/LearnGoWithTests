1e3113908d88f664dbcc78c608d24e8c8b19074c
Hopertz <lugano.ngulwa@gmail.com>
2022-12-22 13:01:35 +0300

    Add use of channels in checkwebsite func

--- Concurrency/check_website.go
+++ Concurrency/check_website.go
@@ -1,19 +1,23 @@
 package Concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 	for _, url := range urls {
-
 		go func(u string) {
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
-
 	}
-	time.Sleep(2 * time.Second)
-
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
 	return results
 }
