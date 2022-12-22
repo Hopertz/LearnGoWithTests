6c711a8f15fec1260b18f9e85993dfa977538279
Hopertz <lugano.ngulwa@gmail.com>
2022-12-22 06:13:07 +0300

    Add check website & concurrency tests

---
+++ Concurrency/check_website_benchmark_test.go
@@ -0,0 +1,21 @@
+package Concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
